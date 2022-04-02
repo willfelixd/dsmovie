5dec4d2b5cffeadb52542b9ec35693da811a2286
William Felix <willfelixd@gmail.com>
2022-04-02 15:14:53 -0300

    Atualização Geral PARTE 5

--- menu.js
+++
@@ -1,9 +0,0 @@
-let show = true
-
-const menuSection = document.querySelector(".menu-section")
-const menuBurguer = menuSection.querySelector(".menu-burguer")
-
-menuBurguer.addEventListener("click", () =>{
-    menuSection.classList.toggle("on", show)
-    show = !show
-})
